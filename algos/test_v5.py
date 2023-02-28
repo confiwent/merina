02e9cc75812663299f4cd08e177d1f0e2279aa66
confiwent <kanif@outlook.com>
2023-02-28 15:03:49 +0800

    greedy policy

--- algos/test_v5.py
+++ algos/test_v5.py
@@ -106,9 +106,14 @@ def evaluation(actor_net, vae_net, log_path_ini, test_env, \
             with torch.no_grad():
                 latent = vae_net.get_latent(ob_).detach()
                 prob = actor_net.forward(state_, latent).detach()
-            action = prob.multinomial(num_samples=1).detach()
-            bit_rate = int(action.squeeze().cpu().numpy())
-            # bit_rate = int(torch.argmax(prob).squeeze().cpu().numpy())
+            
+            ## NOTICES: you can choose either the greedy or stochastic policy output as you like
+            # stochastic policy
+            # action = prob.multinomial(num_samples=1).detach()
+            # bit_rate = int(action.squeeze().cpu().numpy())
+
+            # greedy policy
+            bit_rate = int(torch.argmax(prob).squeeze().cpu().numpy())
             
             if end_of_video:
                 ob = np.zeros((vae_in_channels, c_len)) 
